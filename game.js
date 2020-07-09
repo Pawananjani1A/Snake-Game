23e77daba10d8c09a07d7e3e9498ce4fea74c0bd
Pawananjani1A <pawananjanimth1@gmail.com>
2020-07-09 13:00:29 +0530

    Doubled Canvas size

--- game.js
+++ game.js
@@ -11,59 +11,59 @@ function init()
     pen = canvas.getContext('2d');
     W = canvas.width;
     H = canvas.height;
-    
-    
+
+
     food = getRandomFood();
      score = 5;
     game_over = false;
-    
+
     snake = {
         init_length:5,
         color:"yellow",
         cells:[],
         direction:"right",
-        
+
         createSnake:function(){
             for(var i=this.init_length-1;i>=0;--i)
                 {
                     this.cells.push({x:i,y:0});
                 }
         },
-        
+
         drawSnake:function(){
             for(var i=0;i<this.cells.length;++i)
-                {  
-                    
-                    
+                {
+
+
                     pen.fillStyle = this.color;
-                    
+
                     pen.strokeStyle = "black";
                     pen.lineWidth = 5;
-                    
+
                     pen.strokeRect(this.cells[i].x*10,this.cells[i].y*10,10,10);
                     pen.fillRect(this.cells[i].x*10,this.cells[i].y*10,10,10);
 
                 }
         },
-        
+
         updateSnake:function(){
             var headX = this.cells[0].x;
             var headY = this.cells[0].y;
-            
-            
-            
-            
+
+
+
+
             if(headX==food.x && headY == food.y)
                 {
                    food = getRandomFood();
                     score++;
-                    
+
                 }
-                
+
             else
                 this.cells.pop(); //Pop the last cell if food not eaten
-            
-            
+
+
             if(this.direction=="right")
                 {
                    nextHeadX = headX+1;
@@ -84,32 +84,31 @@ function init()
                     nextHeadX = headX;
                     nextY = headY-1;
                 }
-        
+
             // Insertion at head
             this.cells.unshift({x:nextHeadX,y:nextY});
-            
+
             //Find out the last coordinate(boundaries)
             var last_x = Math.round(W/10);
             var last_y = Math.round(H/10);
-            
-            if(this.cells[0].y<0 || this.cells[0].x<0 
-              || this.cells[0].x>last_x || this.cells[0].y>last_y)
+
+            if(this.cells[0].y<0 || this.cells[0].x<0 || this.cells[0].x>last_x || this.cells[0].y>last_y)
                 {
                     alert("Game Over !");
                     game_over = true;
                 }
-            
+
         }
     };
-    
+
     snake.createSnake();
-    
+
 //    Add eventListeners to our game
 //    Listen for keyboard events
     function keyPressed(e)
     {
         console.log("You pressed a key ");
-        
+
         if(e.key=="ArrowRight")
             snake.direction = "right";
         else if(e.key=="ArrowLeft")
@@ -126,14 +125,14 @@ function init()
 
 
 function draw()
-{      
+{
        pen.clearRect(0,0,W,H);
       snake.drawSnake();
-    
+
     //Lets draw the food
     pen.fillStyle = food.color;
     pen.fillRect(food.x*10,food.y*10,10,10);
-    
+
     pen.fillStyle = "white";
     pen.font = "14px Roboto";
     pen.fillText("Score : "+score,10,10);
@@ -141,8 +140,8 @@ function draw()
 }
 
 function update()
-{       
-      
+{
+
        snake.updateSnake();
 }
 
@@ -150,7 +149,7 @@ function gameLoop()
 {
     draw();
     update();
-    
+
     if(game_over==true)
         {
            clearInterval(f);
@@ -161,16 +160,16 @@ function getRandomFood()
 {
     var foodX = Math.round(Math.random()*(W-10)/10);
     var foodY = Math.round(Math.random()*(H-10)/10);
-    
+
     foodColors = ["red","green","aqua","coral","orchid"];
     var i = Math.round(Math.random()*foodColors.length);
-    
+
     var food = {
         x:foodX,
         y:foodY,
         color:foodColors[i]
     };
-    
+
     return food;
 }
 
